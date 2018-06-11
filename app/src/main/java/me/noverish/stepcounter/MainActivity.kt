c85337646e56a4f99782d717c97b13b510fea602
Noverish Harold <embrapers263@gmail.com>
2018-06-11 20:57:00 +0900

    Finished Step Counter

--- app/src/main/java/me/noverish/stepcounter/MainActivity.kt
+++ app/src/main/java/me/noverish/stepcounter/MainActivity.kt
@@ -1,12 +1,62 @@
 package me.noverish.stepcounter
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import me.noverish.stepcounter.step.StepDetector
+import me.noverish.stepcounter.step.StepListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener, StepListener {
+
+    private var simpleStepDetector: StepDetector? = null
+    private var sensorManager: SensorManager? = null
+    private var numSteps: Int = 0
+    private var isStarted: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get an instance of the SensorManager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        simpleStepDetector = StepDetector()
+        simpleStepDetector!!.registerListener(this)
+
+        step_counter_start_btn.setOnClickListener {
+            isStarted = !isStarted
+
+            if (isStarted) {
+                step_counter_start_btn.text = getString(R.string.step_counter_stop)
+                sensorManager!!.registerListener(this, sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST)
+            } else {
+                step_counter_start_btn.text = getString(R.string.step_counter_start)
+                sensorManager!!.unregisterListener(this)
+            }
+
+        }
+
+        step_counter_reset_btn.setOnClickListener {
+            numSteps = 0
+            step_label.text = getString(R.string.init_step_number)
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector!!.updateAccelerometer(event.timestamp, event.values[0], event.values[1], event.values[2])
+        }
+    }
+
+    override fun step(timeNs: Long) {
+        step_label.text = (++numSteps).toString()
     }
 }
